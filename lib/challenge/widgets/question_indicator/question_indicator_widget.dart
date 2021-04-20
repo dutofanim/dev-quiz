6b71d77d1edf55dcf5f658c1e29f65f167ca1918
dutofanim <etofanim@gmail.com>
2021-04-20 19:35:33 -0300

    Initiated Challenge page.

---
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class QuestionIndicatorWidget extends StatelessWidget {
+  const QuestionIndicatorWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      child: Column(
+        children: [
+          Text("Questão 04"),
+          Text("de 10"),
+        ],
+      ),
+    );
+  }
+}
