0abeb9cdae1bbb67dd356de581957b7cb1613a0b
dutofanim <etofanim@gmail.com>
2021-04-26 14:47:09 -0300

    Added changes to quizzes feedback.

--- lib/challenge/widgets/awnser/awnser_widget.dart
+++ lib/challenge/widgets/awnser/awnser_widget.dart
@@ -1,34 +1,35 @@
 import 'package:devquiz/core/core.dart';
+import 'package:devquiz/shared/models/awnser_model.dart';
 import 'package:flutter/material.dart';
 
 class AwnserWidget extends StatelessWidget {
-  final String title;
-  final bool rightAwnser;
+  final AwnserModel awnser;
   final bool selected;
+  final VoidCallback onTap;
 
   const AwnserWidget(
       {Key? key,
-      required this.title,
-      this.rightAwnser = false,
+      required this.awnser,
+      required this.onTap,
       this.selected = false})
       : super(key: key);
 
   Color get _selectedColorRight =>
-      rightAwnser ? AppColors.darkGreen : AppColors.darkRed;
+      awnser.rightAwnser ? AppColors.darkGreen : AppColors.darkRed;
 
   Color get _selectedBorderRight =>
-      rightAwnser ? AppColors.lightGreen : AppColors.lightRed;
+      awnser.rightAwnser ? AppColors.lightGreen : AppColors.lightRed;
 
   Color get _selectedColorCardRight =>
-      rightAwnser ? AppColors.lightGreen : AppColors.lightRed;
+      awnser.rightAwnser ? AppColors.lightGreen : AppColors.lightRed;
 
   Color get _selectedBorderCardRight =>
-      rightAwnser ? AppColors.green : AppColors.red;
+      awnser.rightAwnser ? AppColors.green : AppColors.red;
 
   TextStyle get _selectedTextStyleRight =>
-      rightAwnser ? AppTextStyles.bodyDarkGreen : AppTextStyles.bodyDarkRed;
+      awnser.rightAwnser ? AppTextStyles.bodyDarkGreen : AppTextStyles.bodyDarkRed;
 
-  IconData get _selectedIconRight => rightAwnser ? Icons.check : Icons.close;
+  IconData get _selectedIconRight => awnser.rightAwnser ? Icons.check : Icons.close;
 
   @override
   Widget build(BuildContext context) {
@@ -37,43 +38,46 @@ class AwnserWidget extends StatelessWidget {
         horizontal: 16,
         vertical: 4,
       ),
-      child: Container(
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: selected ? _selectedColorCardRight : AppColors.white,
-          border: Border.fromBorderSide(
-            BorderSide(
-              color: selected ? _selectedBorderCardRight : AppColors.border,
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: selected ? _selectedColorCardRight : AppColors.white,
+            border: Border.fromBorderSide(
+              BorderSide(
+                color: selected ? _selectedBorderCardRight : AppColors.border,
+              ),
             ),
+            borderRadius: BorderRadius.circular(10),
           ),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Text(
-                title,
-                style: selected ? _selectedTextStyleRight : AppTextStyles.body,
-              ),
-            ),
-            Container(
-              height: 24,
-              width: 24,
-              decoration: BoxDecoration(
-                color: selected ? _selectedColorRight : AppColors.white,
-                border: Border.fromBorderSide(
-                  BorderSide(
-                    color: selected ? _selectedBorderRight : AppColors.border,
-                  ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Text(
+                  awnser.title,
+                  style: selected ? _selectedTextStyleRight : AppTextStyles.body,
                 ),
-                borderRadius: BorderRadius.circular(500),
               ),
-              child: selected
-                  ? Icon(_selectedIconRight, color: AppColors.white, size: 16)
-                  : null,
-            )
-          ],
+              Container(
+                height: 24,
+                width: 24,
+                decoration: BoxDecoration(
+                  color: selected ? _selectedColorRight : AppColors.white,
+                  border: Border.fromBorderSide(
+                    BorderSide(
+                      color: selected ? _selectedBorderRight : AppColors.border,
+                    ),
+                  ),
+                  borderRadius: BorderRadius.circular(500),
+                ),
+                child: selected
+                    ? Icon(_selectedIconRight, color: AppColors.white, size: 16)
+                    : null,
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,11 +1,21 @@
 import 'package:devquiz/challenge/widgets/awnser/awnser_widget.dart';
 import 'package:devquiz/core/core.dart';
+import 'package:devquiz/shared/models/awnser_model.dart';
 import 'package:devquiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
-class  QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
   const QuizWidget({Key? key, required this.question}) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AwnserModel awnser(int index) => widget.question.awnsers[index];
 
   @override
   Widget build(BuildContext context) {
@@ -16,18 +26,20 @@ class  QuizWidget extends StatelessWidget {
             height: 64,
           ),
           Text(
-            question.title,
+            widget.question.title,
             style: AppTextStyles.heading15,
           ),
           SizedBox(
             height: 24,
           ),
-          ...question.awnsers.map(
-            (e) => AwnserWidget(
-              rightAwnser: e.rightAwnser,
-              title: e.title,
-            ),
-          ).toList(),
+          for (var i = 0; i < widget.question.awnsers.length; i += 1)
+            AwnserWidget(
+                awnser: awnser(i),
+                selected: indexSelected == i,
+                onTap: () {
+                  indexSelected = i;
+                  setState(() {});
+                }),
         ],
       ),
     );
